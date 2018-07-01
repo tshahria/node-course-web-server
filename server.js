5fb0095d6ea879bbff7e72ebd6fbcdf80c500214
Tanvir <34381989+tshahria@users.noreply.github.com>
2018-06-30 23:33:29 -0400

    Fixed some files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app =express();
 
@@ -60,6 +61,6 @@ app.get('/bad',(request,response) =>{
   });
 });
 
-app.listen(3000, ()=>{
-  console.log("app is up on server 3000")
+app.listen(port, ()=>{
+  console.log("app is up on server "+ port)
 });
